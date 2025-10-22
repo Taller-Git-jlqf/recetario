aa465ce07dd62b5dec702b4f5be6e5d9c7c8381a
America Alejandra Gracida Hernandez <amegra01@gmail.com>
2025-10-22 10:41:50 -0600

    Agregar receta de helado opcion 2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from recetas import receta_pasta
 from recetas import receta_sandwich
 from recetas import receta_costillas
-
+from recetas import receta_helado
 
 # Aquí se irán importando más recetas a medida que se agreguen
 
 def mostrar_menu():
     print("Recetario disponible:")
     print("1. Pasta al ajo")
+    print("2. Helado de vainilla")
     print("5. Costillas de cerdo a la BBQ")
     print("6. Sandwich de jamon")
 
@@ -17,6 +18,8 @@ def mostrar_menu():
 
     if opcion == "1":
         receta_pasta()
+    elif opcion == "2":
+        receta_helado()
     elif opcion == "6":
         receta_sandwich()
     elif opcion == "5":
