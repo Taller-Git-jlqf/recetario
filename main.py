cdb92685b5546806170412a57c8582c75077fbfc
Enoc Elias Pérez Matías <enoc.elias37@gmail.com>
2025-10-22 08:49:10 -0600

    feat: Agregado receta Costillas en opción 5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from recetas import receta_pasta
+from recetas import receta_costillas
 
 
 # Aquí se irán importando más recetas a medida que se agreguen
@@ -6,12 +7,15 @@ from recetas import receta_pasta
 def mostrar_menu():
     print("Recetario disponible:")
     print("1. Pasta al ajo")
+    print("5. Costillas de cerdo a la BBQ")
     # Agrega aquí tu receta con un número nuevo
 
     opcion = input("Elige una receta (número): ")
 
     if opcion == "1":
         receta_pasta()
+    elif opcion == "5":
+        receta_costillas()
     else:
         print("Opción no válida. Intenta de nuevo.")
 
